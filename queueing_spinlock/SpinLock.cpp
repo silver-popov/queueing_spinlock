58afae1f05268fc79ea117fabd511c5b671fedb3
Igor Popov <silver.popov@gmail.com>
2020-11-09 09:45:16 +0300

    Initial commit.

---
+++ queueing_spinlock/SpinLock.cpp
@@ -0,0 +1,47 @@
+﻿// Queued spin lock algorithm based on:
+// Algorithms for Scalable Synchronization on
+// Shared Memory Multiprocessors
+// John M. Mellor-Crummey, Michael L. Scott
+// http://www.cs.rice.edu/~johnmc/papers/tocs91.pdf
+
+#include "SpinLock.h"
+
+namespace queueing_spinlock {
+
+using namespace std;
+
+inline
+void SpinLock::acquire (SpinLockNode& node) noexcept
+{
+	node.sn_next_ = nullptr;
+	SpinLockNode* predecessor = last_.exchange (&node);
+	if (predecessor) {
+		// Queue was non-empty
+		node.sn_locked_ = 1;
+		predecessor->sn_next_ = &node;
+		while (node.sn_locked_)
+			;
+	}
+}
+
+inline
+void SpinLock::release (SpinLockNode& node) noexcept
+{
+	if (!node.sn_next_) {
+		// No known successor
+		for (;;) {
+
+			// If this node is last, clear and return
+			SpinLockNode* pnode = &node;
+			if (last_.compare_exchange_weak (pnode, nullptr))
+				return;
+
+			// Wait for a successor
+			if (node.sn_next_)
+				break;
+		}
+	}
+	node.sn_next_->sn_locked_ = 0; // Unlock the successor
+}
+
+}
---
+++ queueing_spinlock/SpinLock.h
@@ -0,0 +1,49 @@
+﻿// Queueing spin lock algorithm based on:
+// Algorithms for Scalable Synchronization on
+// Shared Memory Multiprocessors
+// John M. Mellor-Crummey, Michael L. Scott
+// http://www.cs.rice.edu/~johnmc/papers/tocs91.pdf
+
+#ifndef QUEUEINGSPINLOCK_H_
+#define QUEUEINGSPINLOCK_H_
+
+#include <atomic>
+
+namespace queueing_spinlock {
+
+class SpinLock;
+
+class SpinLockNode
+{
+	SpinLockNode (const SpinLockNode&) = delete;
+	SpinLockNode& operator = (const SpinLockNode&) = delete;
+public:
+	SpinLockNode () noexcept :
+		sn_locked_ (0)
+	{}
+
+private:
+	friend class SpinLock;
+	volatile int sn_locked_;
+	SpinLockNode* volatile sn_next_;
+};
+
+class SpinLock
+{
+	SpinLock (const SpinLock&) = delete;
+	SpinLock& operator = (const SpinLock&) = delete;
+public:
+	SpinLock () noexcept :
+		last_ (nullptr)
+	{}
+
+	void acquire (SpinLockNode& node) noexcept;
+	void release (SpinLockNode& node) noexcept;
+
+private:
+	std::atomic <SpinLockNode*> last_;
+};
+
+}
+
+#endif
